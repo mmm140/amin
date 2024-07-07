492db4e4938281fdbc339d8c674cb262e619e230
mmm140 <kh.malekan@chmail.ir>
2024-07-07 21:19:27 +0330

    modify fullscreen view

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,14 +1,19 @@
 #include "Game.h"
+#include <QScreen>
+#include <QApplication>
 
 Game::Game() {
     view = new QGraphicsView();
+    view->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    view->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    QScreen *screen = QApplication::primaryScreen();
+    view->setFixedSize(screen->geometry().width(), screen->geometry().height());
     view->showFullScreen();
 
     scene = new QGraphicsScene(view);
     view->setScene(scene);
-//    scene->setSceneRect(0, 0, view->width(), view->height());
+    scene->setSceneRect(0, 0, view->width(), view->height());
     scene->setBackgroundBrush(QBrush("#124552"));
-    scene->addRect(QRect(0,0,view->width(),view->height()), QPen(QBrush("red"), 5, Qt::DashLine), QBrush("yellow"));
 }
 
 Game::~Game() {
