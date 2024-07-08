915a36df4eade729954e1911108874ec467d9969
Mohsen ghalibaf <mohsenmmm.ggg@gmail.com>
2024-07-08 23:52:32 +0330

    handleBackGroundMovement updated

--- src/Game.cpp
+++ src/Game.cpp
@@ -107,13 +107,20 @@ void Game::closeGameFunction() {
 
 void Game::handleBackGroundMovement()
 {
+    static int t=time(0)+2;
+
+    if(time(0)>=t)
+    {
     srand(time(0));
     rand() % ((int) scene->width());
 
     auto hill = new Decorator(scene, speed);
     connect(player, &Player::BGMovement, hill, &Decorator::handle_leftMovement);
     connect(player, &Player::BGStop, hill, &Decorator::stopMovement);
-    hill->position = Position(0,0);
+    hill->position = Position(Decoration.end()[0]+rand() % ((int) scene->width()),0);
     hill->draw(scene);
     decoration.push_back(hill);
+    t=time(0)+2;
+    }
+
 }
